e30380831c804b0610caab867edde8d7921e2324
Onyinye Okongwu <onyilove95@gmail.com>
2022-09-12 05:21:09 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers of base 10 starting from 0
+ *
+ * Return: 0 (Success)
+ */
+
+int main(0)
+{
+	int a;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		putchar(a)
+	}
+	
+	putchar('\n');
+
+	return (0);
+}
