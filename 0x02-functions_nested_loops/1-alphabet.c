0849994a7c414700f16389514209610aa6f6d4e8
Onyinye Okongwu <onyilove95@gmail.com>
2022-09-13 14:25:57 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - prints the alphabet in lower case
+ *
+ * Retun: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		putchar('alphabet');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
