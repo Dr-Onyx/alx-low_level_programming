3767df0fb507918e84388c8865b8ef0a092f0d45
Onyinye Okongwu <onyilove95@gmail.com>
2022-09-14 01:03:06 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,7 +10,7 @@
 void print_times_table(int n)
 {
 	int x, y, z;
-	
+
 	if (n >= 0 && n <= 14)
 	{
 		for (x = 0; x <= n; x++)
@@ -22,7 +22,7 @@ void print_times_table(int n)
 				if (z > 99)
 				{
 					_putchar(',');
-					
+
 					_putchar(32);
 
 					_putchar((z / 100) + '0');
