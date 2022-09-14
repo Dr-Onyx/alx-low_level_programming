fdb202d987fad8125e589d50ef614bfad0ba697b
Onyinye Okongwu <onyilove95@gmail.com>
2022-09-13 17:27:54 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_times_table - prints the n times table, starting with 0
+ * print_times_table - prints the multiplication table up to param
  *
  * @n: number to be treated
  *
