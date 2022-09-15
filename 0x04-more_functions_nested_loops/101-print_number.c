92447b0339236f5109a7b8bd331602b5d32ebec2
Onyinye Okongwu <onyilove95@gmail.com>
2022-09-15 13:40:43 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -2,9 +2,23 @@
 
 /**
  * print_number - prints an integer
+ * @n: number to be printed
  *
  * Return: always 0
  */
 
 void print_number(int n)
 {
+	unsigned int i;
+
+	i = n;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		x = -n;
+	}
+	if (x / 10)
+		print_number(x / 10);
+	_putchar((x % 10) + '0');
+}
