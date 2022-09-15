c2e4fdb2dc4a657a93a3c7ef6fcabd380d667c21
Onyinye Okongwu <onyilove95@gmail.com>
2022-09-15 12:34:13 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,3 +13,32 @@
 
 int main(void)
 {
+	int i = 1;
+
+	while (i <= 100)
+	{
+		if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+		if (i != 100)
+		{
+			_putchar(' ');
+		}
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
