5e81a60c7da7a46f5a54fe1749387a36196e11d8
Onyinye Okongwu <onyilove95@gmail.com>
2022-09-15 12:59:04 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,3 +9,28 @@
 
 void print_triangle(int size)
 {
+	int i = 1, j;
+
+	while (i <= size && size > 0)
+	{
+		j = 0;
+		while (j < size - 1)
+		{
+			_putchar(' ');
+
+			j++;
+		}
+		j = 0;
+		while (j < i)
+		{
+			_putchar('#');
+
+			j++;
+		}
+
+		_putchar('\n');
+		i++;
+	}
+	if (i == 1)
+		_putchar('\n');
+}
