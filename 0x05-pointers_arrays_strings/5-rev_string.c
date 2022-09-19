f7b96dd1e55a1315d63dd45bf04489bd513514e5
Onyinye Okongwu <onyilove95@gmail.com>
2022-09-19 04:26:06 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,11 +2,23 @@
 
 /**
  * rev_string - reverses a string
+ * @s: string to be reversed
  *
  * Return: void
  */
 
 void rev_string(char *s)
 {
-	char
+	int i = 0, len = 0;
+	char tmp;
+
+	while (s[i++])
+		len++;
+
+	for (i = len - 1; i >= len / 2; i--)
+	{
+		tmp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = tmp;
+	}
 }
