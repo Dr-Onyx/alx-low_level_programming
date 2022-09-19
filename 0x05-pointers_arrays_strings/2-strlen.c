b481ebc89ad61a3020ebfcb981ffab2da2f7cc39
Onyinye Okongwu <onyilove95@gmail.com>
2022-09-19 03:57:42 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,15 +4,16 @@
 /**
  * _strlen - returns the length of a string
  * @s: string
- * 
+ *
  * Return: length
  */
 
 int _strlen(char *s)
 {
-	char len = 0;
+	int len = 0;
 
 	while (*s++)
 		len++;
+
 	return (len);
 }
