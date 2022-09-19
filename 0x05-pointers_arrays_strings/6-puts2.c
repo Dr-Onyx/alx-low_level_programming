fe65865e4e091b625663990e86256e35bd16b058
Onyinye Okongwu <onyilove95@gmail.com>
2022-09-19 06:02:31 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,13 +9,15 @@
 
 void puts2(char *str)
 {
-	int i, len = 0;
+	int i;
 
-	while (str[i++])
-		len++;
-
-	for (i = 0; i < len; i += 2)
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
 		_putchar(str[i]);
+		i++;
+	}
 
 	_putchar('\n');
 }
