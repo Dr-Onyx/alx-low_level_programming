f619b869e0a0b34e0e6a23d81525c9423fe6c475
Onyinye Okongwu <onyilove95@gmail.com>
2022-09-19 04:39:54 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -2,12 +2,26 @@
 
 /**
  * puts_half - prints half of a string
- * @n: .......
+ * @str: string
  *
  * Return: void
  */
 
 void puts_half(char *str)
 {
-	char
+	int i = 0, len = 0, j;
+
+	while (str[i++])
+		len++;
+
+	if ((len % 2) == 0)
+		j = len / 2;
+
+	else
+		j = (len + 1) / 2;
+
+	for (i = j; i < len; i++)
+		_putchar(str[i]);
+
+	_putchar('\n');
 }
