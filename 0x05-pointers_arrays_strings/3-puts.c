25d94882768835e4385467cc04e47e9f7b014f80
Onyinye Okongwu <onyilove95@gmail.com>
2022-09-19 04:01:09 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,11 +2,15 @@
 
 /**
  * _puts - prints a string, followed by a new line
+ * @str: string
  *
  * Return: void
  */
 
 void _puts(char *str)
 {
-	char
+	while (*str)
+		_putchar(*str++);
+
+	_putchar('\n');
 }
