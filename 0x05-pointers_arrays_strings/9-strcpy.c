baee092e431bc913230214ed6e023ad6b9e96bff
Onyinye Okongwu <onyilove95@gmail.com>
2022-09-19 04:50:09 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -2,4 +2,20 @@
 
 /**
  * *_strcpy - copies the string
+ * @dest: buffer to copy the string to
+ * @src: the source string to copy
  *
+ * Return: a pointer to the destination string
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	return (dest);
