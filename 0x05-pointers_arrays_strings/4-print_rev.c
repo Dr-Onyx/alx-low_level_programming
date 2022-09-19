6b9cdfd1ee92bcf942ae059b29bd9e9f30926715
Onyinye Okongwu <onyilove95@gmail.com>
2022-09-19 04:05:30 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,11 +2,20 @@
 
 /**
  * print_rev - prints a string, in reverse
+ * @s: string
  *
  * Return: void
  */
 
 void print_rev(char *s)
 {
-	char
+	int len = 0, i;
+
+	while (s(i++))
+		len++;
+
+	for (i = len - 1; i >= 0; i--)
+		_putchar(s(i));
+
+	_putchar('\n');
 }
