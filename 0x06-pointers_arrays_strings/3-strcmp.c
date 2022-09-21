13eee6b5f4dbb4cbad5698c5f25c8554fd8e86c3
Onyinye Okongwu <onyilove95@gmail.com>
2022-09-21 15:28:03 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,7 +4,6 @@
  * _strcmp - compares two strings
  * @s1: first string
  * @s2: second string
- *
  * Return: an integer (0 if equal, +ve if s1 > s2, _ve if s1 < s2)
  */
 
@@ -16,7 +15,7 @@ int _strcmp(char *s1, char *s2)
 	{
 		if (*(s1 + count2) == '\0' && *(s2 + count2) == '\0')
 			break;
-		count = *(s1 + count2 - *(s2 + count2);
+		count = *(s1 + count2) - *(s2 + count);
 		count2++;
 	}
 
