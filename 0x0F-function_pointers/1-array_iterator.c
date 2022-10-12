d4df1beca8f726c54db8d1976dfd8f29480d5485
Onyinye Okongwu <onyilove95@gmail.com>
2022-10-12 01:23:05 -0700

    1-array_iterator.c

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+#include <stddef.h>
+
+/**
+ * array_iterator - applies function to an array of items
+ * @array: an array of integers
+ * @size: size of the array
+ * @action: pointer to the function to be applied
+ *
+ * Return: Null void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (array == NULL || size <= 0 || action == NULL)
+		return;
+
+	for (i = 0; i < size; i ++)
+		action(array[i]);
+}
