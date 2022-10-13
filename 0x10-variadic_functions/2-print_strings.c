4635611fa38321b917c7d75cd7f54e5b06b5d5a3
Onyinye Okongwu <onyilove95@gmail.com>
2022-10-13 16:51:07 -0700

    2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -26,7 +26,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_end(ap);
 
 	va_start(ap, n);
-	for (i = 0, i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		if (arr[i] == 1)
 			printf("(nil)");
