b80dd83105888ac96b425641fda4756d7abb276e
Onyinye Okongwu <onyilove95@gmail.com>
2022-10-13 17:52:34 -0700

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -28,24 +28,24 @@ void print_all(const char * const format, ...)
 		}
 		switch (format[i])
 		{
-			case 'c':
-				printf("%c", va_arg(ap, int)), c = 1;
+		case 'c':
+			printf("%c", va_arg(ap, int)), c = 1;
+			break;
+		case 'i':
+			printf("%i", va_arg(ap, int)), c = 1;
+			break;
+		case 'f':
+			printf("%f", va_arg(ap, double)) c = 1;
+			break;
+		case 's':
+			str = va_arg(ap, char *), c = 1;
+			if (!str)
+			{
+				printf("(nil)");
 				break;
-			case 'i':
-				printf("%i", va_arg(ap, int)), c = 1;
-				break;
-			case 'f':
-				printf("%f", va_arg(ap, double)) c = 1;
-				break;
-			case 's':
-				str = va_arg(ap, char *), c = 1;
-				if (!str)
-				{
-					printf("(nil)");
-					break;
-				}
-				printf("%s", str);
-				break;
+			}
+			printf("%s", str);
+			break;
 		} i++;
 	}
 	printf("\n"), va_end(ap);
